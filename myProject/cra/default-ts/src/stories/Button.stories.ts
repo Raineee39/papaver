a6b481c4a4056a89b949a8988da8f87cdf2526db
lorraine-xel <lorraine@xel.nl>
2024-05-31 15:51:55 +0200

    FIX V2

--- myProject/cra/default-ts/src/stories/Button.stories.ts
+++ myProject/cra/default-ts/src/stories/Button.stories.ts
@@ -1,9 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import { Button } from "./Button";
-import { url } from "inspector";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// Default export for story metadata
 const meta = {
   title: "Example/Button",
   component: Button,
@@ -12,23 +11,19 @@ const meta = {
       type: "figma",
       url: "https://www.figma.com/design/XSpk2L7ZrA3Wj7Y1oBx820/Relume-Library-Figma-Kit-(v1.11)---edited-color-palettes?node-id=3480-7762&t=wYvEv50Jgafa8XlO-1",
     },
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     backgroundColor: { control: "color" },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+// Story definitions
 export const Primary: Story = {
   args: {
     primary: true,
