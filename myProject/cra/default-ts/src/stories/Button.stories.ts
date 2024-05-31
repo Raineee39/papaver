df869c6aacb6650cce5874d54511959841148d2e
lorraine-xel <lorraine@xel.nl>
2024-05-31 15:55:16 +0200

    ANOTHER TEST!

--- myProject/cra/default-ts/src/stories/Button.stories.ts
+++ myProject/cra/default-ts/src/stories/Button.stories.ts
@@ -3,14 +3,10 @@ import { fn } from "@storybook/test";
 import { Button } from "./Button";
 
 // Default export for story metadata
-const meta = {
+const meta: Meta<typeof Button> = {
   title: "Example/Button",
   component: Button,
   parameters: {
-    design: {
-      type: "figma",
-      url: "https://www.figma.com/design/XSpk2L7ZrA3Wj7Y1oBx820/Relume-Library-Figma-Kit-(v1.11)---edited-color-palettes?node-id=3480-7762&t=wYvEv50Jgafa8XlO-1",
-    },
     layout: "centered",
   },
   tags: ["autodocs"],
@@ -18,7 +14,7 @@ const meta = {
     backgroundColor: { control: "color" },
   },
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
